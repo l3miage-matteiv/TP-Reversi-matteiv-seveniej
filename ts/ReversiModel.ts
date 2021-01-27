e23e113e1098b9db4e4e4613acc0b4a46369d831
l3miage-matteiv <77281307+l3miage-matteiv@users.noreply.github.com>
2021-01-27 20:08:56 +0100

    Constructor done

--- ts/ReversiModel.ts
+++ ts/ReversiModel.ts
@@ -11,10 +11,16 @@ export class ReversiModel implements ReversiModelInterface {
 
     constructor() {
         this.initBoard();
+
+        this.gameStateSubj = new BehaviorSubject<GameState>({
+            board: this.board,
+            turn: this.currentTurn
+        });
+        this.gameStateObs = this.gameStateSubj.asObservable();
     }
 
     initBoard(): void {
-        // Remplir tous les lignes avec 'empty'
+        /* Remplir tous les lignes avec 'empty'
         const B = [];                       // [L[],L[]]
         for(let i = 0; i < 8; i++) {
             const L : C[] = [];             // [C['Empty'|Turn],...,C['Empty'|Turn]] 
@@ -24,17 +30,24 @@ export class ReversiModel implements ReversiModelInterface {
             B.push(L);
         }
         this.board = B as Board;
+        ou*/
+        this.board =
+            new Array(8).fill(0).map(
+                ()=> new Array<C>(8).fill("Empty")
+            ) as Board;
 
         this.board[3][3] = this.board[4][4] = 'Player2';
         this.board[3][4] = this.board[4][3] = 'Player1';
 
         this.currentTurn = 'Player1';
-
     }
 
     PionsTakenIfPlayAt(x: number, y: number): PlayImpact {
         // Parcourir les 8 directions pour accumuler les coordonnées de pions prenables
+        // Cherche que les cases dans une des directions contiennent n pions de l'autre joueur et aussi qu'il rencontre un pion a lui a la fin
 
+        // Commencer par parcourir une direction
+        // 
         return [];
     }
 
